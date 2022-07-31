acadf6f9cb80049b3b2b49d5a01cbd423dec9af4
Brian <BMArsenault@gmail.com>
2022-07-30 17:57:07 -0700

    Added search form to newspage/homepage

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -9,7 +9,9 @@ const Nav = ({currentPage, handlePageChange}) => {
                     <div className="justify-content-end">
                     <ul className="nav nav-tabs navbar-text navbar-right mb-2">
                     <li className="nav-item d-flex flex-column text-center">
-                    {/*<a href="#" onClick={() => {handlePageChange('News')}} className={currentPage === 'News' ? 'nav-link active' : 'nav-link'}>News</a>*/}
+                    {/*<a href="#" 
+                    onClick={() => {handlePageChange('News')}} 
+                    className={currentPage === 'News' ? 'nav-link active' : 'nav-link'}>News</a>*/}
                     </li>
                     <li className="nav-item d-flex flex-column text-center">
                     {/*<a href="#" onClick={() => {handlePageChange('About')}} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About Us</a>*/}
--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { newsSearch } from '../../utils/api'
 
 const News = () => {
+    // create state for holding returned news api data
+    const [searchedNews, setSearchedNews] = useState([]);
+    // create state for holding search field data
+    const [searchInput, setSearchInput] = useState('');
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!searchInput) {
+            return false;
+        }
+
+        try {
+            const response = await newsSearch(searchInput);
+            
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const { articles } = await response.json();
+
+            const newsData = articles.map((news) => ({
+                title: news.title,
+                author: news.author,
+                description: news.description,
+                date: news.publishedAt,
+                url: news.url,
+            }));
+
+            setSearchedNews(newsData);
+            setSearchInput('');
+        }   catch (err) {
+            console.error(err);
+        }
+    };
+    
     return (
-        <div className='news-search'>
-            <div className='card'>
-                <div className='card-title'>
-                    <div className='card-body text-center'>
-                    </div>
-                </div>
-            </div>
+        <>
+        <div className='container search'>
+            <h1>Search the News</h1>
+            <form onSubmit={handleFormSubmit}>
+                <input
+                name='searchInput'
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                type='text'
+                placeholder='Search news'/>
+                <button type='submit'>Search</button>
+            </form>
         </div>
+        <div>
+            <h2>
+            {searchedNews.length
+            ? `Viewing ${searchedNews.length} results:`
+            : 'Search news topic to begin'}
+            </h2>
+            
+        </div>
+
+        </>
     )
 }
 
